6870616dd95a2d9b38871f350775d2654376ffd0
RetroFriends <lostastronaut@rocketship.com>
2020-11-01 10:46:55 -0500

    Updating copyright and license notice

--- Apolune/Map.h
+++ Apolune/Map.h
@@ -1,3 +1,17 @@
+/*********************************************************************************************
+*  __    __________________   ________________________________   __________  ________       *
+* /\ \  /\  __ \  ___\__  _\ /\  __ \  ___\__  _\  == \  __ \ "-.\ \  __ \ \/\ \__  _\ (tm) *
+* \ \ \_\_\ \/\ \___  \/\ \/ \ \  __ \___  \/\ \/\  __<\ \/\ \ \-.  \  __ \ \_\ \/\ \/      *
+*  \ \_____\_____\_____\ \_\  \ \_\ \_\_____\ \_\ \_\ \_\_____\_\\"\_\_\ \_\_____\ \_\      *
+*   \/_____/_____/_____/\/_/   \/_/\/_/_____/\/_/\/_/\/_/_____/_/ \/_/_/\/_/_____/\/_/      *
+*    --------------------------------------------------------------------------------       *
+*     Lost Astronaut Game Development Framework (c) 2007-2020 H. Elwood Gilliland III       *
+*********************************************************************************************
+* This software is copyrighted software.  Use of this code is given only with permission to *
+* parties who have been granted such permission by its author, Herbert Elwood Gilliland III *
+* This software is licensed by permissions of the following open source LICENSE:  AGPL 3.0  *
+* See https://www.gnu.org/licenses/agpl-3.0.en.html for details on this license.            *
+*********************************************************************************************/
 #pragma once
 #include "Cartesian.h"
 #include "GLWindow.h"
